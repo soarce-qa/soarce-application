276421a3db670fd23fbecfad57fda3c9b5a6e49f
Holger Segnitz <holger@segnitz.net>
2019-07-25 23:27:52 +0200

    ADD: web-gui for controlling usecases

---
+++ src/application/Controllers/ControlController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Soarce\Application\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Container;
+use Soarce\Control\Usecase;
+
+class ControlController
+{
+    /** @var Container */
+    protected $ci;
+
+    /**
+     * BaseController constructor.
+     *
+     * @param Container $dependencyInjectionContainer
+     */
+    public function __construct(Container $dependencyInjectionContainer)
+    {
+        $this->ci = $dependencyInjectionContainer;
+    }
+
+    /**
+	 * @param  Request $request
+	 * @param  Response $response
+	 * @return Response
+	 */
+  	public function index(Request $request, Response $response): Response
+    {
+        return $this->ci->view->render($response, 'control/index.twig');
+	}
+
+    /**
+     * @param  Request  $request
+     * @param  Response $response
+     * @param  array    $args
+     * @return Response
+     */
+	public function usecase(Request $request, Response $response, $args): Response
+    {
+        $usecaseControl = new Usecase($this->ci);
+        $usecase = $args['usecase'] ?? '';
+        $action  = $request->getParam('action');
+
+        if (null !== $usecase && $request->isPost()) {
+            switch ($action) {
+                case 'create':
+                    $usecaseControl->create($request->getParam('usecase'));
+                    break;
+                case 'activate':
+                    $usecaseControl->activate($usecase);
+                    break;
+                case 'restart':
+                    $usecaseControl->restart($usecase);
+                    break;
+                case 'delete':
+                    $usecaseControl->delete($usecase);
+                    break;
+            }
+        }
+
+        $this->ci->view['usecases'] = $usecaseControl->getAllUsecases();
+
+        return $this->ci->view->render($response, 'control/usecase.twig');
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -6,7 +6,11 @@ $container = require '../application/Bootstrap.php';
 
 $app = new App($container);
 
-$app->get('/',        '\Soarce\Application\Controllers\IndexController:index');
-$app->any('/receive', '\Soarce\Application\Controllers\ReceiveController:index');
+$app->get('/',         '\Soarce\Application\Controllers\IndexController:index');
+$app->any('/receive',  '\Soarce\Application\Controllers\ReceiveController:index');
+$app->group('/control', function() {
+    $this->get('',                      '\Soarce\Application\Controllers\ControlController:index');
+    $this->any('/usecases[/{usecase}]', '\Soarce\Application\Controllers\ControlController:usecase');
+});
 
 $app->run();
