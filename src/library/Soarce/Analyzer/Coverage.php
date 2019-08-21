ee411f40bcbe38ada3b5f22177a653e5c1e8f391
Holger Segnitz <holger@segnitz.net>
2019-08-21 22:43:56 +0200

    UPD: database queries to reflect coverage type info

--- src/library/Soarce/Analyzer/Coverage.php
+++ src/library/Soarce/Analyzer/Coverage.php
@@ -18,11 +18,11 @@ class Coverage extends AbstractAnalyzer
     {
         $sql = 'SELECT a.`id` as `applicationId`, a.`name` as `applicationName`, f.`id` as `fileId`, f.`filename` as `fileName`, COUNT(distinct c.`line`) as `lines`
             FROM `file`        f
-            JOIN `application` a ON a.`id`             = f.`application_id` ' . ($application !== null ? " and a.`id`         = {$application} " : '') . '
-            JOIN `request`     r ON r.`application_id` = a.`id` ' . ($usecase !== null ? " and r.`usecase_id` = {$usecase} " : '') . ($request !== null ? " and r.`id`         = {$request} " : '') . '
-            JOIN `coverage`    c ON c.`request_id`     = r.`id` and c.`file_id` = f.`id`
+            JOIN `application` a ON a.`id`             = f.`application_id` ' . ($application !== null ? " and a.`id` = {$application} " : '') . '
+            JOIN `request`     r ON r.`application_id` = a.`id` ' . ($usecase !== null ? " and r.`usecase_id` = {$usecase} " : '') . ($request !== null ? " and r.`id` = {$request} " : '') . '
+            JOIN `coverage`    c ON c.`request_id`     = r.`id` and c.`file_id` = f.`id` and c.`covered` = 1
             WHERE 1
-            GROUP BY a.name, f.filename
+            GROUP BY a.id, f.filename
             ORDER BY a.name ASC, f.filename ASC';
         $result = $this->mysqli->query($sql);
 
@@ -70,12 +70,13 @@ class Coverage extends AbstractAnalyzer
      */
     public function getCoverage($fileId, $usecaseId = null, $requestId = null): array
     {
-        $sql = 'SELECT c.`line`
+        $sql = 'SELECT c.`line`, MAX(c.`covered`) as `coverageLevel`
             FROM `coverage` c
-            JOIN `request`  r ON r.`id` = c.`request_id`'
+            JOIN `request`  r ON r.`id` = c.`request_id` '
                 . ($usecaseId !== null ? " and r.`usecase_id` = {$usecaseId} " : '')
                 . ($requestId !== null ? " and r.`id` = {$requestId} " : '') . '
-            WHERE c.`file_id` = ' . (int)$fileId;
+            WHERE c.`file_id` = ' . (int)$fileId . '
+            GROUP BY c.`line` ';
 
         $result = $this->mysqli->query($sql);
 
@@ -85,7 +86,7 @@ class Coverage extends AbstractAnalyzer
 
         $ret = [];
         while ($row = $result->fetch_assoc()) {
-            $ret[$row['line']] = true;
+            $ret[$row['line']] = $row['coverageLevel'];
         }
 
         return $ret;
@@ -120,7 +121,7 @@ class Coverage extends AbstractAnalyzer
         $sql = 'SELECT DISTINCT r.`id`, r.`request_id`
             FROM `coverage` c 
             JOIN `request`  r on c.`request_id` = r.`id`
-            WHERE c.`file_id` = ' . (int)$fileId . ' AND c.`line` = ' . (int)$line . '
+            WHERE c.`file_id` = ' . (int)$fileId . ' AND c.`line` = ' . (int)$line . ' AND c.`covered` = 1
             ORDER BY r.`request_id` ASC';
 
         $ret = [];
@@ -147,7 +148,7 @@ class Coverage extends AbstractAnalyzer
             FROM `coverage` c 
             JOIN `request`  r on c.`request_id` = r.`id`
             JOIN `usecase`  u on r.`usecase_id` = u.`id`
-            WHERE c.`file_id` = ' . (int)$fileId . ' AND c.`line` = ' . (int)$line . '
+            WHERE c.`file_id` = ' . (int)$fileId . ' AND c.`line` = ' . (int)$line . ' AND c.`covered` = 1
             ORDER BY r.`request_id` ASC';
 
         $ret = [];
