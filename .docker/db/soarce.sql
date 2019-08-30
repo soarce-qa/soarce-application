41dfe89a5d1a0d0ad070c47614ae093210149874
Holger Segnitz <holger@segnitz.net>
2019-08-31 00:25:53 +0200

    ADD: new column and table for function map feature

--- .docker/db/soarce.sql
+++ .docker/db/soarce.sql
@@ -70,24 +70,38 @@ CREATE TABLE `file` (
 -- Data exporting was unselected.
 
 -- Dumping structure for table soarce.function_call
-CREATE TABLE IF NOT EXISTS `function_call` (
-                                               `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                                               `file_id` int(10) unsigned NOT NULL,
-                                               `request_id` mediumint(8) unsigned NOT NULL,
-                                               `class` varchar(382) DEFAULT NULL,
-                                               `function` varchar(382) NOT NULL,
-                                               `type` enum('internal','user-defined') NOT NULL,
-                                               `calls` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-                                               `walltime` FLOAT UNSIGNED NOT NULL DEFAULT '0',
-                                               PRIMARY KEY (`id`),
-                                               UNIQUE KEY `file_id_class_function` (`file_id`,`request_id`,`class`,`function`),
-                                               KEY `fi__files` (`file_id`),
-                                               KEY `FK_function_call_request` (`request_id`),
-                                               CONSTRAINT `FK__files` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-                                               CONSTRAINT `FK_function_call_request` FOREIGN KEY (`request_id`) REFERENCES `request` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+CREATE TABLE `function_call` (
+                                 `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                                 `file_id` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+                                 `request_id` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
+                                 `class` VARCHAR(382) NULL DEFAULT NULL,
+                                 `function` VARCHAR(382) NOT NULL,
+                                 `type` ENUM('internal','user-defined') NOT NULL,
+                                 `calls` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+                                 `walltime` FLOAT UNSIGNED NOT NULL DEFAULT '0',
+                                 `number` MEDIUMINT(8) UNSIGNED NULL DEFAULT NULL,
+                                 PRIMARY KEY (`id`),
+                                 UNIQUE INDEX `file_id_class_function` (`file_id`, `request_id`, `class`, `function`),
+                                 INDEX `fi__files` (`file_id`),
+                                 INDEX `request2` (`request_id`),
+                                 INDEX `number` (`number`),
+                                 CONSTRAINT `file2` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+                                 CONSTRAINT `request2` FOREIGN KEY (`request_id`) REFERENCES `request` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+)   COLLATE='utf8mb4_general_ci' ENGINE=InnoDB ROW_FORMAT=DYNAMIC AUTO_INCREMENT=1;
 
 -- Data exporting was unselected.
+
+CREATE TABLE `function_map` (
+                                `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                                `caller` BIGINT(20) UNSIGNED NOT NULL,
+                                `callee` BIGINT(20) UNSIGNED NOT NULL,
+                                `calls` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+                                PRIMARY KEY (`id`),
+                                INDEX `caller` (`caller`),
+                                INDEX `callee` (`callee`),
+                                CONSTRAINT `callee` FOREIGN KEY (`callee`) REFERENCES `function_call` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+                                CONSTRAINT `caller` FOREIGN KEY (`caller`) REFERENCES `function_call` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 -- Dumping structure for table soarce.request
 CREATE TABLE IF NOT EXISTS `request` (
