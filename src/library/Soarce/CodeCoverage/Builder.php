e4fcefa746d0f12450bf6f951c4423c324d83a8f
Holger Segnitz <holger@segnitz.net>
2025-06-11 00:42:17 +0200

    UPD coverage now lists usecases from source
    -> this should be great in the combined coverage reports

--- src/library/Soarce/CodeCoverage/Builder.php
+++ src/library/Soarce/CodeCoverage/Builder.php
@@ -6,45 +6,49 @@ use SebastianBergmann\CodeCoverage\CodeCoverage;
 use SebastianBergmann\CodeCoverage\Data\ProcessedCodeCoverageData;
 use SebastianBergmann\CodeCoverage\Data\RawCodeCoverageData;
 use SebastianBergmann\CodeCoverage\Filter;
+use Soarce\Analyzer\Coverage;
 use Soarce\CodeCoverage\Driver\DummyDriver;
 
 class Builder
 {
-    public function __construct(private \mysqli $mysqli)
+    public function __construct(private \mysqli $mysqli, private Coverage $coverageAnalyzer)
     {}
 
     public function getCodeCoverage(int $applicationId): CodeCoverage
     {
-        $rawCoverage = $this->collectCoverageFromDb($applicationId);
+        $primaryCodeCoverage = new CodeCoverage(
+            new DummyDriver(),
+            new Filter()
+        );
 
-        $driver = new DummyDriver();
+        foreach ($this->coverageAnalyzer->getUsecases([], [], [$applicationId]) as $usecaseId => $usecase) {
+            $rawCoverage = $this->collectCoverageFromDb($applicationId, $usecaseId);
 
-        $filter = new Filter();
-        $reflection = new \ReflectionClass($filter);
+            $rawCodeCoverageData = RawCodeCoverageData::fromXdebugWithoutPathCoverage($rawCoverage);
 
-        $files = array_keys($rawCoverage);
-        $files = array_combine($files, array_fill(0, count($files), true));
+            $processed = new ProcessedCodeCoverageData();
+            $processed->initializeUnseenData($rawCodeCoverageData);
+            $processed->markCodeAsExecutedByTestCase('soarce:' . $usecase['name'], $rawCodeCoverageData);
 
-        $reflection->getProperty('files')->setValue($filter, $files);
-        $reflection->getProperty('isFileCache')->setValue($filter, $files);
+            $codeCoverage = new CodeCoverage(
+                new DummyDriver(),
+                new Filter()
+            );
 
-        $codeCoverage = new CodeCoverage($driver, $filter);
+            $codeCoverage->setData($processed);
+            $primaryCodeCoverage->merge($codeCoverage);
+        }
 
-        $rawCodeCoverageData = RawCodeCoverageData::fromXdebugWithoutPathCoverage($rawCoverage);
-
-        $processed = new ProcessedCodeCoverageData();
-        $processed->initializeUnseenData($rawCodeCoverageData);
-        $processed->markCodeAsExecutedByTestCase('soarce', $rawCodeCoverageData);
-
-        $codeCoverage->setData($processed);
-        return $codeCoverage;
+        return $primaryCodeCoverage;
     }
 
-    private function collectCoverageFromDb(int $applicationId): array
+    private function collectCoverageFromDb(int $applicationId, int $usecaseId): array
     {
         $sql = "SELECT any_value(f.filename) as `filename`, c.line, max(c.covered) as `covered` 
-            FROM `file` f JOIN coverage c ON f.id = c.file_id
-            WHERE f.application_id = " . $applicationId . "
+            FROM `file` f
+                JOIN coverage c ON f.id = c.file_id
+                JOIN request r on c.request_id = r.id
+            WHERE f.application_id = " . $applicationId . " AND r.usecase_id = " . $usecaseId . "
             GROUP BY c.line
             ORDER BY `filename`, c.line";
 
